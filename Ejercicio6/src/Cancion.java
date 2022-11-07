511cfdbbd26993bfeb602b3691c0a3edde4b2689
chuy-zip <74995966+chuy-zip@users.noreply.github.com>
2022-11-07 08:28:00 -0600

    Added variables to classes

--- Ejercicio6/src/Cancion.java
+++ Ejercicio6/src/Cancion.java
@@ -1,4 +1,74 @@
 
-public class Cancion {
+public class Cancion implements ICancion{
+	
+	private String Title;
+	private String Artist;
+	private String Album;
+	private String Duration;
+	private int ID;
+	
+	public Cancion() {
+		
+	}
+
+	@Override
+	public void setTitle(String _title) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setArtist(String _artist) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getArtist() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setAlbum(String _album) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getAlbum() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDuration(String _duration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getDuration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setID(int _id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- Ejercicio6/src/IPodController.java
+++ Ejercicio6/src/IPodController.java
@@ -1,4 +1,115 @@
 
-public class IPodController {
+public class IPodController implements IIpod_simulator{
+	private boolean ON;
+	private boolean Blocked;
+	private ICancion[] My_Songs;
+	
+	
+	public IPodController() {
+		
+	}
+
+	@Override
+	public boolean SwitchONOFF(boolean actual_state) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean LockUnlockDevice(boolean actual_locked_state) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public float getVolume() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float setVolume(float volume) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int Prev(int actual_index) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int Next(int actual_index) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getActualIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setActualIndex(int actual_index) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addToFavorite(ICancion _song) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ICancion selectSpecificSong(int index) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ICancion selectSpecificFavoriteSong(int index) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ICancion[] getAllSongs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getStatus(boolean _isON, boolean _isLocked, boolean _isPlaying, ICancion _actualSong) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isValidIndex(int index) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void addSongToList(String _titulo, String _artista, String _album, String _duracion, int _id)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteSongFromList(int index) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteSongFromTop10(int index) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
